9e9510a2758e51bbf4b6611dc6cb12f6e2fe0d78
Tobias Lundin <tobias.lundin@gmail.com>
2015-07-22 12:44:57 +0200

    fix tests

--- test/teamcity_tests.js
+++ test/teamcity_tests.js
@@ -10,11 +10,11 @@ exports.teamcity = {
     exec('grunt myTask --no-color', function (err, taskOutput) {    
 			exec('grunt teamcity:myTask --no-color', function (err, result) {
         
-        test.ok(/##teamcity\[blockOpened name=\"myTask\"\]/.test(result), 'should contain opening block statement');
-        test.ok(/##teamcity\[blockClosed name=\"myTask\"\]/.test(result), 'should contain closing block statement');
+        test.ok(/##teamcity\[blockOpened name=\'myTask\''\]/.test(result), 'should contain opening block statement');
+        test.ok(/##teamcity\[blockClosed name=\'myTask\''\]/.test(result), 'should contain closing block statement');
         
-        result = result.replace('##teamcity\[blockOpened name="myTask"]', '[#]');
-        result = result.replace('##teamcity\[blockClosed name="myTask"]', '[#]');
+        result = result.replace("##teamcity\[blockOpened name='myTask']", '[#]');
+        result = result.replace("##teamcity\[blockClosed name='myTask']", '[#]');
         
         test.ok(result.split('[#]')[1].trim() === taskOutput.trim(), 'should add result inside block');
         
@@ -26,9 +26,9 @@ exports.teamcity = {
     test.expect(3);
 		exec('grunt teamcity:multiTask --no-color', function (err, result) {
       
-      test.ok(/##teamcity\[blockOpened name=\"myTask\"\]/.test(result), 'add opening block statement for myTask');
-      test.ok(/##teamcity\[blockOpened name=\"myAsyncTask\"\]/.test(result), 'add opening block for myAsyncTask');
-      test.ok(/##teamcity\[blockOpened name=\"myFinalTask\"\]/.test(result), 'add opening block for myFinalTask');
+      test.ok(/##teamcity\[blockOpened name=\'myTask\''\]/.test(result), 'add opening block statement for myTask');
+      test.ok(/##teamcity\[blockOpened name=\'myAsyncTask\''\]/.test(result), 'add opening block for myAsyncTask');
+      test.ok(/##teamcity\[blockOpened name=\'myFinalTask\''\]/.test(result), 'add opening block for myFinalTask');
       
       test.done();
 		});
@@ -37,7 +37,7 @@ exports.teamcity = {
     test.expect(1);
     exec('grunt teamcity:withOpts --no-color', function (err, result) {
       
-      test.ok(/##teamcity\[blockOpened name=\"myPreFixTo myTask\"\]/.test(result), 'adds name prefix before task name');
+      test.ok(/##teamcity\[blockOpened name=\'myPreFixTo myTask\'\]/.test(result), 'adds name prefix before task name');
       
       test.done();
 		});
