08b95061582449721f932619185ce8fba2be6076
Tobias Lundin <tobias.lundin@gmail.com>
2015-07-22 12:46:06 +0200

    fix typo with double single quotes

--- test/teamcity_tests.js
+++ test/teamcity_tests.js
@@ -10,8 +10,8 @@ exports.teamcity = {
     exec('grunt myTask --no-color', function (err, taskOutput) {    
 			exec('grunt teamcity:myTask --no-color', function (err, result) {
         
-        test.ok(/##teamcity\[blockOpened name=\'myTask\''\]/.test(result), 'should contain opening block statement');
-        test.ok(/##teamcity\[blockClosed name=\'myTask\''\]/.test(result), 'should contain closing block statement');
+        test.ok(/##teamcity\[blockOpened name=\'myTask\'\]/.test(result), 'should contain opening block statement');
+        test.ok(/##teamcity\[blockClosed name=\'myTask\'\]/.test(result), 'should contain closing block statement');
         
         result = result.replace("##teamcity\[blockOpened name='myTask']", '[#]');
         result = result.replace("##teamcity\[blockClosed name='myTask']", '[#]');
@@ -26,9 +26,9 @@ exports.teamcity = {
     test.expect(3);
 		exec('grunt teamcity:multiTask --no-color', function (err, result) {
       
-      test.ok(/##teamcity\[blockOpened name=\'myTask\''\]/.test(result), 'add opening block statement for myTask');
-      test.ok(/##teamcity\[blockOpened name=\'myAsyncTask\''\]/.test(result), 'add opening block for myAsyncTask');
-      test.ok(/##teamcity\[blockOpened name=\'myFinalTask\''\]/.test(result), 'add opening block for myFinalTask');
+      test.ok(/##teamcity\[blockOpened name=\'myTask\'\]/.test(result), 'add opening block statement for myTask');
+      test.ok(/##teamcity\[blockOpened name=\'myAsyncTask\'\]/.test(result), 'add opening block for myAsyncTask');
+      test.ok(/##teamcity\[blockOpened name=\'myFinalTask\'\]/.test(result), 'add opening block for myFinalTask');
       
       test.done();
 		});
